52425f6398984fd3acefe3edc726eae17be36a77
terngkub <terngnattapol@gmail.com>
2018-01-28 01:59:42 +0100

    finish lecture 63

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var SpaceHipster = SpaceHipster || {};
+
+SpaceHipster.game = new Phaser.Game('100%', '100%', Phaser.AUTO);
+
+SpaceHipster.game.state.add('GameState', SpaceHipster.GameState);
+SpaceHipster.game.state.start('GameState');
